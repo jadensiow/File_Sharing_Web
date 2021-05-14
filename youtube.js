5f7c17e3f14394a61a151bb03a5312c198d893ca
jadensiow <78722564+jadensiow@users.noreply.github.com>
2021-05-15 02:27:41 +0800

    Delete youtube.js

--- youtube.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "http://18.136.106.220:8000",
-});
-CMD[("python", "backend/manage.py", "makemigrations", "accounts")];
-CMD[("python", "backend/manage.py", "makemigrations", "videos")];
-CMD[("python", "backend/manage.py", "migrate", "accounts")];
-CMD[("python", "backend/manage.py", "migrate", "videos")];
